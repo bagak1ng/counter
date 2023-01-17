e014d2f8d8c15a7ed92ff23934163f3158504322
bagak1ng <malinasamat@gmail.com>
2023-01-17 12:27:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const handlePlus = () => {
+    const plus = counter + 1;
+    setCounter(plus);
+  }
+
+  const handleMinus = () => {
+    const minus = counter - 1;
+    setCounter(minus);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{counter}</h1>
+        <button className="minus" onClick={handleMinus}>- Минус</button>
+        <button className="plus" onClick={handlePlus}>Плюс +</button>
       </div>
     </div>
   );
